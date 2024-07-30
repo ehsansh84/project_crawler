9b1c0bc156dad2926590e2b592ec4d5646b59743
ehsanyl <163835521+ehsanyl@users.noreply.github.com>
2024-07-30 22:11:04 +0200

    added: publics.py

--- publics.py
+++ publics.py
@@ -1,7 +1,22 @@
 from dotenv import load_dotenv
+import os
+import hashlib
+
 load_dotenv()
 
 
 def db():
     from tinydb import TinyDB, Query
     return TinyDB('db.json')
+
+
+def mdb():
+    from pymongo import MongoClient
+    con = MongoClient(os.getenv('MONGO_URL'))
+    return con[os.getenv('DB_NAME')]
+
+
+def create_hash(s):
+    hash_object = hashlib.sha256()
+    hash_object.update(s.encode('utf-8'))
+    return hash_object.hexdigest()
